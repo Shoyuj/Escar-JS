540ded49f432cd0c6cbaff563e86807f8ed16015
Shoyuj <shoyujsharma@gmail.com>
2025-02-01 14:15:25 +0000

    nums and maths done

---
+++ basics/nums.js
@@ -0,0 +1,17 @@
+let num = new Number(100)
+console.log(num);
+
+const num2 = 10000000
+console.log(num2.toLocaleString('en-IN'));
+
+const num3 =217.63725
+
+console.log(num3.toFixed(2));
+console.log(num3.toPrecision(4));
+
+min = 1
+max = 6
+
+console.log(Math.floor(Math.random()*(max-min+1)+min))
+
+
