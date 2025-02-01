15bca09588b1840275f4b2c6f21b987fadeeb495
Shoyuj <shoyujsharma@gmail.com>
2025-02-01 13:22:59 +0000

    strings done

---
+++ basics/strings.js
@@ -0,0 +1,16 @@
+let name ="Escar"
+let no = 50
+
+console.log(`Hello my name is ${name} and my no is ${no}`);
+
+let n1 = name.substring(0,3)
+console.log(n1);
+
+let n2 = name.slice(-4,4)
+console.log(n2);
+
+url = "escar-fiod-fkjsd"
+
+console.log(url.split('-'));
+
+
